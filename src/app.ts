aa5fc1a7a72ae24b116fbf594a1a57347f25b3cc
Amaresh Pati <amaresh.development@gmail.com>
2026-03-19 15:33:48 +0530

    feat: Initialize Fastify application with logger, request ID generation, and integrate Swagger documentation.

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import Fastify from 'fastify';
+import { config } from './config/env.js';
+import { randomUUID } from 'node:crypto';
+import swaggerPlugin from './plugins/swagger.plugin.js';
+import userRoutes from './modules/user/user.routes.js';
+import healthRoutes from './modules/health/health.routes.js';
+
+export const buildConfigs = async () => {
+    const fastify = Fastify({
+        logger: {
+            level: config.LOG_LEVEL,
+            // will active this later 
+            // redact: [
+            //     'headers.authorization',
+            //     'headers.cookie',
+            //     'body.password',
+            //     'body.token',
+            //     'body.accessToken'
+            // ],
+
+            transport: {
+                target: 'pino-pretty',
+                options: {
+                    colorize: true,
+                    translateTime: 'SYS:HH:MM:ss',
+                    ignore: 'pid,hostname',
+                },
+            },
+        },
+        genReqId: (req) => {
+            // header best practice: don't use "x-" https://www.rfc-editor.org/info/rfc6648 and keep it lowercase
+            return (req.headers['request-id'] as string) ?? randomUUID();
+        },
+        routerOptions: {
+            ignoreDuplicateSlashes: true,
+        },
+        ajv: {
+            customOptions: {
+                keywords: ['example'],
+            },
+        },
+    });
+
+    // Register plugins
+    await fastify.register(swaggerPlugin);
+
+    // Register routes
+    await fastify.register(healthRoutes, { prefix: '/' });
+    await fastify.register(userRoutes, { prefix: '/api' });
+
+    return fastify;
+}
